24c479f280861d101ea47e32c87cf4146a585cb9
TrongThang <quandh@deha-soft.com>
2023-04-14 09:18:39 +0700

    up

---
+++ BT1.js
@@ -0,0 +1,10 @@
+let diemLy = +prompt("nhập điểm vật lí");
+let diemhoa = +prompt("nhập điểm hóa học");
+let diemsinh = +prompt("nhập điểm sinh học");
+let diemtrungbinh = ( diemLy + diemhoa + diemsinh )/3;
+let tongdiem = diemLy + diemhoa + diemsinh;
+
+
+document.write("điểm trung bình: "+ diemtrungbinh);
+document.write<br>
+document.write("điểm tổng: "+ tongdiem);
